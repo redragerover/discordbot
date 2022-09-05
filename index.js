450b4736bea028ef1f86a94eca8a2086641f65c4
redragerover <red_rage_rover@protonmail.com>
2022-09-05 07:10:43 -0400

    refactor pa bot to be simpler

--- index.js
+++ index.js
@@ -1,102 +1,68 @@
-import Discord from "discord.js"
-import dotenv from "dotenv"
+import Discord from "discord.js";
+import dotenv from "dotenv";
 
-import { getLiveVideoURLFromChannelID, twitterUrlPurifier } from "./urlUtils.js"
-import { handleDoubleCheck, handleStreameIsRemainingOnline, handleStreamerIsOn } from "./ytLiveState.js"
+import { handleYouTubePoll } from "ytlivemanager";
+import {
+  twitterUrlPurifier,
+  getLiveVideoURLFromChannelID,
+} from "ytlivemanager/urlUtils.js";
+dotenv.config();
 
-const toSeconds = seconds => seconds * 1000
-const toMinutes = minutes => 60_000 * minutes
-const nodeArguments = process.argv
-dotenv.config()
-const token = process.env.discord_token
-const guildId = process.env.discord_serverId
-const channelId = process.env.discordChannelId
-const ytChannelId = process.env.youtube_channelId
-
-const pollingIntervalTimer = toSeconds(37)
-const timeToDelayCheck = toMinutes(50)
+const token = process.env.discord_token;
+const guildId = process.env.discord_serverId;
+const channelId = process.env.discordChannelId;
+const ytChannelId = process.env.youtube_channelId;
 
 const client = new Discord.Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_BANS",
-        "GUILD_MESSAGE_TYPING"
-    ]
-})
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_BANS", "GUILD_MESSAGE_TYPING"],
+});
 
 client.on("messageCreate", async (message) => {
-    const lowerCaseCommand = message.content.toLowerCase()
+  if (message.author.bot) {
+    return;
+  }
+  const lowerCaseCommand = message.content.toLowerCase();
 
-    const { purifiedTwitterUrl } = twitterUrlPurifier(lowerCaseCommand)
-    if (purifiedTwitterUrl) {
-        message.suppressEmbeds(true)
-        message.reply(purifiedTwitterUrl)
-    }
+  const { purifiedTwitterUrl } = twitterUrlPurifier(lowerCaseCommand);
+  if (purifiedTwitterUrl) {
+    message.suppressEmbeds(true);
+    message.reply(purifiedTwitterUrl);
+  }
 
-    if (lowerCaseCommand.includes("!live")) {
-        const { canonicalURL } = await getLiveVideoURLFromChannelID(ytChannelId);
-        message.reply(canonicalURL)
-        return;
-    }
-})
+  if (lowerCaseCommand.includes("!troop")) {
+    message.send("FUCK U TROOP");
+    return;
+  }
+
+  if (lowerCaseCommand.includes("!live")) {
+    const { canonicalURL } = await getLiveVideoURLFromChannelID(ytChannelId);
+    message.reply(canonicalURL);
+    return;
+  }
+});
 
 client.on("ready", () => {
-    console.log("Project Based Chat is online")
-    const guild = client.guilds.cache.get(guildId);
-    const channel = guild.channels.cache.get(channelId);
+  console.log("Project Based Chat is online");
+  const guild = client.guilds.cache.get(guildId);
+  const channel = guild.channels.cache.get("929303551384117248" || channelId);
 
-    const handleYouTubePoll = () => {
-        const state = {
-            streamerIsOn: false,
-            streamIsAlreadyOnline: nodeArguments.includes("skip"),
-            doubleCheckIfOffline: false,
-            setStreamerIsOn(val) {
-                state.streamerIsOn = val
-            },
-            setStreamIsAlreadyOnline(val) {
-                state.streamIsAlreadyOnline = val
-            },
-            setDoubleCheckIfOffline(val) {
-                console.log(state.doubleCheckIfOffline)
-                state.doubleCheckIfOffline = val
-            },
-        }
-        const handleInterval = async () => {
-            console.log({state})
-            await getLiveVideoURLFromChannelID(ytChannelId).then(({ isStreaming, canonicalURL }) => {
+  const streamToLive = (canonicalURL) => {
+    channel.send(
+      `<@222540413943283712> stream is live @everyone @everyone @everyone ${canonicalURL}`
+    );
+    client.user.setActivity(`PA is online !live`, {
+      type: "PLAYING",
+    });
+  };
+  const streamGoesOffline = () => {
+    client.user.setActivity("PA OFFLINE", { type: "PLAYING" });
+  };
 
-                if(state.streamerIsOn){
-                    return;
-                }
-                handleDoubleCheck(state, () => {
-                    // send streamer has gone permanently offline
-                    // channel.send("PA has gone offline")
-                    client.user.setActivity('stream offline', { type: 'PLAYING' });
-                    
-                }, isStreaming, timeToDelayCheck)
-                if(state.doubleCheckIfOffline){
-                    return
-                }
-                handleStreameIsRemainingOnline(state, isStreaming)
-                if(state.streamIsAlreadyOnline){
-                    return
-                }
+  handleYouTubePoll({
+    identifier: ytChannelId,
+    streamGoesOffline: streamGoesOffline,
+    streamToLive: streamToLive,
+  });
+});
 
-                handleStreamerIsOn(state, () => {
-                    //send message only once
-                    // channel.send(`<@222540413943283712> stream is live @everyone @everyone @everyone ${canonicalURL}`)
-                    console.log("message EVERYONE")
-                    client.user.setActivity('stream online', { type: 'PLAYING' });
-
-                }, isStreaming)
-                return;
-            }).catch(err => console.log(err))
-        }
-
-        const interval = setInterval(handleInterval, pollingIntervalTimer)
-    }
-    handleYouTubePoll()
-})
-
-client.login(token)
+client.login(token);
